e83aa6d365044c66044073ef66f5f7519dfb4752
Jaime D <134320299+jdoerr13@users.noreply.github.com>
2023-06-02 16:06:03 -0400

    adding the exercise

--- reduce.js
+++ reduce.js
@@ -6,7 +6,14 @@ Examples:
     extractValue(arr,'name') // ['Elie', 'Tim', 'Matt', 'Colt']
 */
 
-function extractValue(arr, key) {}
+function extractValue(arr, key) {//map is probably best for this
+  return arr.reduce(function(acc, next){
+  // console.log(acc,next);
+    acc.push(next[key]);//next is the current elemment being processed
+    return acc;//acc holds the result
+  }, []);//[] as second argument
+}
+
 
 /*
 Write a function called vowelCount which accepts a string and returns an object with the keys as the vowel and the values as the number of times the vowel appears in the string. This function should be case insensitive so a lowercase letter and uppercase letter should count
@@ -19,7 +26,20 @@ Examples:
     vowelCount('I Am awesome and so are you') // {i: 1, a: 4, e: 3, o: 3, u: 1};
 */
 
-function vowelCount(str) {}
+function vowelCount(str) {
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    const lowerCaseStr = str.toLowerCase();
+
+    const counts = lowerCaseStr.split('').reduce(function(acc, char){//remember acc holds the result
+       // console.log(acc);
+        if (vowels.includes(char)){
+        acc[char] = acc[char] + 1 || 1;
+        }
+        return acc;
+    },{})
+return counts;
+
+}
 
 /*
 Write a function called addKeyAndValue which accepts an array of objects and returns the array of objects passed to it with each object now including the key and value passed to the function.
@@ -36,7 +56,17 @@ Examples:
        ]
 */
 
-function addKeyAndValue(arr, key, value) {}
+function addKeyAndValue(arr, key, value) {
+    return arr.reduce(function(acc,next){
+        let newObj = {
+            [key]: value, ...next
+    };
+    acc.push(newObj);
+    console.log (acc)
+    return acc;//modified acc object returned
+    
+    },[]);
+}
 
 /*
 Write a function called partition which accepts an array and a callback and returns an array with two arrays inside of it. The partition function should run the callback function on each value in the array and if the result of the callback function at that specific value is true, the value should be placed in the first subarray. If the result of the callback function at that specific value is false, the value should be placed in the second subarray. 
@@ -60,4 +90,13 @@ Examples:
     partition(names, isLongerThanThreeCharacters) // [['Elie', 'Colt', 'Matt'], ['Tim']]
 */
 
-function partition(arr, callback) {}
+function partition(arr, callback) {
+    return arr.reduce(function(acc, value) {
+        if (callback(value)) {// take the values that are true in the call back function,(function call that takes the value as an argument)
+          acc[0].push(value); // Add value to the first subarray
+        } else {
+          acc[1].push(value); // Add value to the second subarray
+        }
+        return acc;
+      }, [[], []]); 
+}
